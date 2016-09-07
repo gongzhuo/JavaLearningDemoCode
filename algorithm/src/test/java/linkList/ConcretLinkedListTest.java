1580790286157350b65bd696b709667558fb5eee
gongzhuo <1296634240@qq.com>
2016-09-07 23:08:25 +0800

    refresh

--- algorithm/src/test/java/linkList/ConcretLinkedListTest.java
+++ algorithm/src/test/java/linkList/ConcretLinkedListTest.java
@@ -1,6 +1,6 @@
 package linkList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,8 +9,8 @@ public class ConcretLinkedListTest {
 	@Test
 	public void testAddNode() {
 		ConcretLinkedList demo = new ConcretLinkedList();
-		
-		for(int i = 0; i < 5; i++){
+
+		for (int i = 0; i < 5; i++) {
 			demo.add(new Node(5 - i));
 		}
 		demo.add(new Node(5));
@@ -18,7 +18,11 @@ public class ConcretLinkedListTest {
 		demo.add(new Node(10));
 		assertTrue(demo.count() == 8);
 		System.out.println(demo);
-		
+
+		for (int j = 0; j < 8; j++) {
+			demo.remove();
+			System.out.println(demo + " length = " + demo.count());
+		}
 	}
 
 }

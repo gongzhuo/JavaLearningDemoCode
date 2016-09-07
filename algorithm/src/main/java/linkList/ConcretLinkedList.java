987f72418b78ddee2c488b7d7840160ebde54df8
gongzhuo <1296634240@qq.com>
2016-09-07 10:36:10 +0800

    add notes for ConcretLinkedList.java

--- algorithm/src/main/java/linkList/ConcretLinkedList.java
+++ algorithm/src/main/java/linkList/ConcretLinkedList.java
@@ -1,28 +1,28 @@
 package linkList;
 
-public class ConcretLinkedList implements OurLinkList{
-	
+public class ConcretLinkedList implements OurLinkList {
+
 	private Node head = null;
 
 	public int add(Node node) {
 		int length = 0;
-		if(head == null){
-			head = node;
-			return length + 1;
+		if (head == null) {
+			head = node; // 增加一个节点
+			return length + 1; // 返回数组长度
 		}
-		
-		Node current;
-		if(head == null){
-			return length;
+
+		Node current; // 节点当前值
+		if (head == null) {
+			return length; // 如果链表首为空，返回数组长度
 		}
 		length++;
-		current = head;
-		while(current.next != null){
+		current = head; // 将链表首赋给当前值
+		while (current.next != null) {
 			length++;
 			current = current.next;
 		}
-		current.next = node;
-		return length++;
+		current.next = node; // 增加节点
+		return length++; // 遍历链表，返回链表新长度
 	}
 
 	public boolean remove() {
@@ -38,16 +38,16 @@ public class ConcretLinkedList implements OurLinkList{
 	public int count() {
 		int length = 0;
 		Node current;
-		if(head == null){
-			return length;
+		if (head == null) {
+			return length; // 如果链表首为空，返回数组长度
 		}
 		length++;
 		current = head;
-		while(current.next != null){
+		while (current.next != null) {
 			length++;
 			current = current.next;
 		}
-		return length;
+		return length; // 遍历链表，返回链表长度
 	}
 
 	public boolean exist(Node node) {
@@ -64,17 +64,17 @@ public class ConcretLinkedList implements OurLinkList{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		Node current = head;
 		String result = "";
-		while(current != null){
-			result += current.toString();
+		while (current != null) {
+			result += current.toString(); // 将链表返回值转化为字符型
 			current = current.next;
 		}
 		return result;
-		
+
 	}
 
 }
